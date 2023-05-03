cd175e99eb63c6af66b0177eef864929d78e9a72
Marcela Fernandes <mam.fernandes@hotmail.com>
2023-05-03 01:41:53 -0600

    Bug fixes

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -14,9 +14,22 @@ puts "----------"
 puts "Please enter a store name:"
 @store_name = $stdin.gets.chomp
 
-@store = Store.create(:name => @store_name)
-
+@store = Store.create(:name => @store_name, :annual_revenue => 10000)
 @store.errors.full_messages.each do |message|
   puts "Couldn't create store: #{message}"
 end
 
+
+puts "Please enter your first name"
+@employee_name = $stdin.gets.chomp
+
+puts "Please enter your last name"
+@employee_name2 = $stdin.gets.chomp
+
+puts "Please enter your rate"
+@employee_rate = $stdin.gets.chomp
+
+@employee = Employee.create(:first_name => @employee_name, :last_name => @employee_name2, :hourly_rate => @employee_rate, :store_id => @store.id)
+@employee.errors.full_messages.each do |message|
+  puts "Couldn't create employee: #{message}"
+end
